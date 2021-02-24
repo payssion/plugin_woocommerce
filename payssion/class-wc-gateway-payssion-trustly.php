ee22f0bf80a511c45519cf46d2fe08cd17e97907
payssion <liudong4u@gmail.com>
2021-02-24 17:36:20 +0800

    add trustly

---
+++ payssion/class-wc-gateway-payssion-trustly.php
@@ -0,0 +1,18 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once( 'class-wc-gateway-payssion.php' );
+
+/**
+ * Payssion 
+ *
+ * @class 		WC_Gateway_Payssion_Trustly
+ * @extends		WC_Payment_Gateway
+ * @author 		Payssion
+ */
+class WC_Gateway_Payssion_Trustly extends WC_Gateway_Payssion {
+	protected $pm_id = 'trustly';
+}
